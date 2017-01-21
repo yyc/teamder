d9e56d38460902f1add997f6c1097970d4e4671f
Yuchuan <yyc478@gmail.com>
2017-01-21 17:57:28 +0800

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var projects = require('projects');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.use('projects', projects)
+
 
 module.exports = router;
